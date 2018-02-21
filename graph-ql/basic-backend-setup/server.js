60845584ade7cd5f225e6cb3c7cd1f77aedf200d
Francesco Rizzi <rizzifrancesco@icloud.com>
2018-02-21 20:13:50 +0100

    RELATED BRANCH: master; MESSAGE: graphql basic-backend-setup

--- graph-ql/basic-backend-setup/server.js
+++ graph-ql/basic-backend-setup/server.js
@@ -1,14 +1,17 @@
 const express        = require('express');
 const expressGraphQL = require('express-graphql');
+const schema         = require('./schema/schema');
 
 const app = express();
 
 
 app.use('/graphql', expressGraphQL({
+	schema,
 	graphiql : true
 }));
 
 
 app.listen(3091, () =>{
-	console.log('Listening...');
+	console.log('Listening on port 3091...');
 });
+
