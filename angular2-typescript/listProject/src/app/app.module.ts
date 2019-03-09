a2b6053b5dad5f6fe76fc5898db97bd8fe545254
Francesco Rizzi <rizzifrancesco@icloud.com>
2019-03-09 22:12:11 +0100

    RELATED BRANCH: master; MESSAGE: angular2+ and typescript pt5

--- angular2-typescript/listProject/src/app/app.module.ts
+++ angular2-typescript/listProject/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
