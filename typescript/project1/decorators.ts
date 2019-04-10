0141d2ab46ef34708ac7146f786b88fb887e1611
Francesco Rizzi <rizzifrancesco@icloud.com>
2019-04-10 22:09:50 +0200

    RELATED BRANCH: master; MESSAGE: in depth typescript pt8

--- typescript/project1/decorators.ts
+++ typescript/project1/decorators.ts
@@ -1,3 +1,39 @@
 //decorators
 ////////////
 
+function logged(constructor: Function) {
+	console.log(constructor);
+}
+
+@logged
+class PersonD {
+	constructor() {
+		console.log('hi');
+	}
+
+}
+
+//factory
+function logging(value: boolean): Function | null {
+	return value ? logged : null;
+}
+
+@logging(true)
+class CarD {
+
+}
+
+//advanced
+function printable(constructor: Function) {
+	constructor.prototype.print = function () {
+		console.log(this);
+	};
+}
+
+@printable
+class PlantD {
+	name = 'green plant';
+}
+
+const plantd = new PlantD();
+(<any>plantd).print(); //TS bug (?) that doesn't recognize the print method
