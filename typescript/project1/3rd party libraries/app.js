42dec25a412b2f789020ce297f46fdd422657df2
Francesco Rizzi <rizzifrancesco@icloud.com>
2019-04-13 19:52:16 +0200

    RELATED BRANCH: master; MESSAGE: in depth typescript pt10

---
+++ typescript/project1/3rd party libraries/app.js
@@ -0,0 +1,11 @@
+//easy way
+/*
+declare var $: any;
+*/
+//manual declaration files
+//see jquery.d.ts
+//declaration files from NPM
+//see node_module/@types
+$('button').click(function () {
+    alert('click');
+});
---
+++ typescript/project1/3rd party libraries/app.ts
@@ -0,0 +1,19 @@
+//easy way
+
+/*
+declare var $: any;
+*/
+
+
+
+//manual declaration files
+//see jquery.d.ts
+
+
+
+//declaration files from NPM
+//see node_module/@types
+
+$('button').click(function () {
+	alert('click');
+});
