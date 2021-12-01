7cfb2f1a8ab847864e8ac5e47e7cfa0505ac9992
Francesco Rizzi <rizzifrancesco@icloud.com>
2021-12-02 00:24:17 +0100

    wip

--- d3/d3-react-experiments/src/components/ExperimentRow.tsx
+++ d3/d3-react-experiments/src/components/ExperimentRow.tsx
@@ -8,8 +8,8 @@ const ExperimentRow: React.FC<ExperimentRowProps> = ({ title, children }) => {
     const [isOpen, setIsOpen] = useState(true);
 
     return (
-        <div className="experiment-row">
-            <div className={`experiment-row-title ${isOpen ? 'modOpen' : null}`} onClick={() => setIsOpen(!isOpen)}>
+        <div className={`experiment-row ${isOpen ? 'modOpen' : null}`} onClick={!isOpen ? () => setIsOpen(true) : undefined}>
+            <div className={`experiment-row-title ${isOpen ? 'modOpen' : null}`} onClick={isOpen ? () => setIsOpen(false) : undefined}>
                 <span>{title}</span> <span className="experiment-row-title-icon">{isOpen ? '▲' : '▼'}</span>
             </div>
             {isOpen && <div className={`experiment-row-content ${isOpen ? 'modOpen' : null}`}>{children}</div>}
