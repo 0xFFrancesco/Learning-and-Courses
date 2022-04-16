9a839b909bd33209e6d33b0a1ea13241ea7223b8
Francesco Rizzi <rizzifrancesco@icloud.com>
2022-04-16 11:22:00 +0200

    Some progress has been made!

--- typescript/course2/fetchjson/index.ts
+++ typescript/course2/fetchjson/index.ts
@@ -3,5 +3,12 @@ import axios from "axios";
 const url = "https://jsonplaceholder.typicode.com/todos/1;";
 
 axios.get(url).then((res) => {
-	console.log(res.data);
+	const todo = res.data;
+	const { ID, Title, finished } = todo;
+
+	console.log(`
+        The todo with ID: ${ID}
+        Has a title of: ${Title}
+        Is it finished? ${finished}    
+    `);
 });
