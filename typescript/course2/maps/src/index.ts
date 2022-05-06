29553da5f90f92e514e24e58827fb9a178e1fcce
Francesco Rizzi <rizzifrancesco@icloud.com>
2022-05-06 23:32:14 +0200

    Some progress has been made!

--- typescript/course2/maps/src/index.ts
+++ typescript/course2/maps/src/index.ts
@@ -1,4 +1,9 @@
+/// <reference types="@types/google.maps" />
+
+import { Company } from "./Company";
 import { User } from "./User";
 
 const user = new User();
-console.log(user.name);
+const company = new Company();
+
+console.log(user, company);
