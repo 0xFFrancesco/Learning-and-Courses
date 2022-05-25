d379fb74cf42ac7f2f7d9342e2682a9652d7a96c
Francesco <francesco.altro@icloud.com>
2022-05-25 18:34:06 +0000

    Some progress has been made!

--- typescript/course2/stats/build/index.js
+++ typescript/course2/stats/build/index.js
@@ -1,2 +1,39 @@
 "use strict";
-console.log("testaaa");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var fs_1 = __importDefault(require("fs"));
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var getData = function () {
+    var matches = fs_1.default.readFileSync("football.csv", {
+        encoding: "utf-8",
+    });
+    var matchesData = matches
+        .split("\n")
+        .map(function (row) { return row.split(","); });
+    return matchesData;
+};
+var analyse = function (matchesData) {
+    var manUnitedWins = 0;
+    for (var _i = 0, matchesData_1 = matchesData; _i < matchesData_1.length; _i++) {
+        var match = matchesData_1[_i];
+        if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+            manUnitedWins++;
+        }
+        else if (match[2] === "Man United" &&
+            match[5] === MatchResult.AwayWin) {
+            manUnitedWins++;
+        }
+    }
+    return manUnitedWins;
+};
+(function () {
+    var results = analyse(getData());
+    console.log("Man. United won ".concat(results, " games."));
+})();
--- typescript/course2/stats/src/index.ts
+++ typescript/course2/stats/src/index.ts
@@ -1,1 +1,39 @@
-console.log("testaaa");
+import fs from "fs";
+
+enum MatchResult {
+	HomeWin = "H",
+	AwayWin = "A",
+	Draw = "D",
+}
+
+const getData = () => {
+	const matches = fs.readFileSync("football.csv", {
+		encoding: "utf-8",
+	});
+
+	const matchesData = matches
+		.split("\n")
+		.map((row: string): string[] => row.split(","));
+
+	return matchesData;
+};
+
+const analyse = (matchesData: string[][]) => {
+	let manUnitedWins = 0;
+	for (let match of matchesData) {
+		if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+			manUnitedWins++;
+		} else if (
+			match[2] === "Man United" &&
+			match[5] === MatchResult.AwayWin
+		) {
+			manUnitedWins++;
+		}
+	}
+	return manUnitedWins;
+};
+
+(() => {
+	const results = analyse(getData());
+	console.log(`Man. United won ${results} games.`);
+})();
