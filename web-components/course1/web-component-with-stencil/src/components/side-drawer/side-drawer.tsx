693671e9224d9214cc9f24a14db5795edbb14156
Francesco <francesco.altro@icloud.com>
2022-06-18 11:40:32 +0000

    Some progress has been made!

---
+++ web-components/course1/web-component-with-stencil/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,23 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'uc-side-drawer',
+  styleUrl: 'side-drawer.css',
+  shadow: true,
+})
+export class SideDrawer {
+  @Prop({ reflect: true }) title: string;
+
+  render() {
+    return (
+      <aside>
+        <header>
+          <h1>{this.title}</h1>
+        </header>
+        <main>
+          <slot />
+        </main>
+      </aside>
+    );
+  }
+}
