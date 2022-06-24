67ed31436063a0776f593cdd0bcc6feec09018a0
Francesco <francesco.altro@icloud.com>
2022-06-24 14:25:17 +0000

    Some progress has been made!

--- micro-frontends/course1/project2/container/config/webpack.dev.js
+++ micro-frontends/course1/project2/container/config/webpack.dev.js
@@ -8,17 +8,16 @@ const packageJson = require("../package.json");
 const devConfig = {
 	mode: "development",
 	devServer: {
-		port: 8081,
+		port: 8080,
 		historyApiFallback: {
 			index: "index.html",
 		},
 	},
 	plugins: [
 		new ModuleFederationPlugin({
-			name: "marketing",
-			filename: "remoteEntry.js",
-			exposes: {
-				"./MarketingApp": "./src/bootstrap",
+			name: "container",
+			remotes: {
+				marketing: "marketing@http://localhost:8081/remoteEntry.js",
 			},
 			shared: packageJson.dependencies,
 		}),
---
+++ micro-frontends/course1/project2/container/src/components/MarketingApp.js
@@ -0,0 +1,13 @@
+import React, { useRef, useEffect } from "react";
+
+import { mount } from "marketing/MarketingApp";
+
+export default () => {
+	const ref = useRef(null);
+
+	useEffect(() => {
+		mount(ref.current);
+	}, []);
+
+	return <div ref={ref}></div>;
+};
