c2e143c58aa54f74b6e8817f469e9c2112d61a43
Francesco <francesco.altro@icloud.com>
2022-06-27 13:05:17 +0000

    Some progress has been made!

--- micro-frontends/course1/project2/marketing/config/webpack.prod.js
+++ micro-frontends/course1/project2/marketing/config/webpack.prod.js
@@ -7,7 +7,7 @@ const packageJson = require("../package.json");
 const prodConfig = {
 	mode: "production",
 	output: {
-		publichPath: "/marketing/latest/",
+		publicPath: "/marketing/latest/",
 		filename: "[name].[contenthash].js",
 	},
 	plugins: [
